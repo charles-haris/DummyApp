32707b6d5762bce336c650597af5b837bcbf956a
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2025-04-17 08:59:06 +0000

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-//declaration 
+  //declaration
   title = 'DummyApp';
   student = {
-   name:"Leo",
-   gender:"Male"
-   };
+    name: "Leo",
+    gender: "Male"
+  };
+
+  countries: string[] = ['Canada', 'France', 'UK']
+  image: string = "https://unsplash.com/fr/photos/une-photo-floue-dun-sapin-de-noel-dans-un-salon-rWJ7sYJHNGQ"
+  count: number = 0
+  inputValue: string = ''
+  value: string = ''
+  fullname: string = ''
+
+
+
+
+  increaseCount() {
+    this.count++;
+  }
+
+  updateValue(event: Event) {
+    this.value = (event.target as HTMLInputElement).value
+    console.log(this.value)
+
+  }
 }
